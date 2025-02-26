f8abd61be62b87ddc1e8c5a0c21e212145d466a0
Aswathi R <a0505177@ti.com>
2025-02-26 22:41:41 +0530

    am261x: mcan: fix am261x-som failures
    - update mcan trx in test_mcan
    - update R5 clock to 400mhz for canfd examples
    
    Fixes: MCUSDK-14245
    Signed-off-by: Aswathi R <a-r1@ti.com>

--- test/drivers/mcan/mcan_lld/am261x-som/test_mcan_transceiver.c
+++ test/drivers/mcan/mcan_lld/am261x-som/test_mcan_transceiver.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2021 Texas Instruments Incorporated
+ *  Copyright (C) 2023-24 Texas Instruments Incorporated
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
@@ -33,72 +33,13 @@
 /* ========================================================================== */
 /*                             Include Files                                  */
 /* ========================================================================== */
-#include <drivers/i2c.h>
+#include <drivers/gpio.h>
+#include <kernel/dpl/AddrTranslateP.h>
 #include "ti_drivers_open_close.h"
 
-/* ========================================================================== */
-/*                           Macros & Typedefs                                */
-/* ========================================================================== */
-
-/* Input status register */
-#define TCA6416_REG_INPUT0              ((UInt8) 0x00U)
-#define TCA6416_REG_INPUT1              ((UInt8) 0x01U)
-
-/* Output register to change state of output BIT set to 1, output set HIGH */
-#define TCA6416_REG_OUTPUT0             ((uint8_t) 0x02U)
-#define TCA6416_REG_OUTPUT1             ((uint8_t) 0x03U)
-
-/* Configuration register. BIT = '1' sets port to input, BIT = '0' sets
- * port to output */
-#define TCA6416_REG_CONFIG0             ((uint8_t) 0x06U)
-#define TCA6416_REG_CONFIG1             ((uint8_t) 0x07U)
-
-/* ========================================================================== */
-/*                          Function Declarations                             */
-/* ========================================================================== */
-static void SetupI2CTransfer(I2C_Handle handle,  uint32_t slaveAddr,
-                      uint8_t *writeData, uint32_t numWriteBytes,
-                      uint8_t *readData,  uint32_t numReadBytes);
+/* Dummy function for am261x-som board as it uses external transceiver */
 
 void test_mcanEnableTransceiver(void)
 {
-    I2C_Handle      i2cHandle;
-    uint8_t         dataToSlave[4];
-
-    i2cHandle = gI2cHandle[CONFIG_I2C0];
-    dataToSlave[0] = TCA6416_REG_CONFIG0;
-    dataToSlave[1] = 0x0U;
-    SetupI2CTransfer(i2cHandle, 0x20, &dataToSlave[0], 1, &dataToSlave[1], 1);
-    /* set the P00 to 0 make them output ports. */
-    dataToSlave[1] &= ~(0x1U);
-    SetupI2CTransfer(i2cHandle, 0x20, &dataToSlave[0], 2, NULL, 0);
-
-    /* Get the port values. */
-    dataToSlave[0] = TCA6416_REG_INPUT0;
-    dataToSlave[1] = 0x0U;
-    SetupI2CTransfer(i2cHandle, 0x20, &dataToSlave[0], 1, &dataToSlave[1], 1);
-
-    /* Set P10 and P11 to 0.
-     */
-    dataToSlave[0] = TCA6416_REG_OUTPUT0;
-    dataToSlave[1] &= ~(0x1);
-    SetupI2CTransfer(i2cHandle, 0x20, &dataToSlave[0], 2, NULL, 0);
-}
-
-static void SetupI2CTransfer(I2C_Handle handle,  uint32_t slaveAddr,
-                      uint8_t *writeData, uint32_t numWriteBytes,
-                      uint8_t *readData,  uint32_t numReadBytes)
-{
-    int32_t status;
-    I2C_Transaction i2cTransaction;
-
-    /* Enable Transceiver */
-    I2C_Transaction_init(&i2cTransaction);
-    i2cTransaction.targetAddress = slaveAddr;
-    i2cTransaction.writeBuf = (uint8_t *)&writeData[0];
-    i2cTransaction.writeCount = numWriteBytes;
-    i2cTransaction.readBuf = (uint8_t *)&readData[0];
-    i2cTransaction.readCount = numReadBytes;
-    status = I2C_transfer(handle, &i2cTransaction);
-    DebugP_assert(SystemP_SUCCESS == status);
+    
 }
